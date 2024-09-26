020932e13c809f63ce2652c1d98f57d1cc25e7a9
farhadctg222 <farhadctg222@gmail.com>
2024-09-26 15:53:39 +0600

    d

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className='bg-teal-200'>
+
+        <p className='text-justify p-10'>আমরা, <span className='text-red-900 font-extrabold'>Join Work.org</span> এর সদস্যরা, এই মর্মে অঙ্গীকার করছি যে আমরা সুবিধাবঞ্চিত শিশুদের জীবনমান উন্নয়নে সর্বোচ্চ নিষ্ঠা, সততা, এবং মানবিকতার সঙ্গে কাজ করবো। <br /><br />
+        <span className='text-bold'>আমাদের প্রতিটি পদক্ষেপে আমরা:</span>
+         
+             </p>
+        <p className='text-justify p-5'> <span className='font-extrabold'>মানবিকতা ও সম্মান: </span>প্রতিটি শিশুর প্রতি মানবিক মূল্যবোধ ও মর্যাদার ভিত্তিতে আচরণ করবো।</p>
+        <p className='text-justify p-5'> <span className='font-extrabold'>সততা ও স্বচ্ছতা: </span>আমাদের সকল কার্যক্রমে পূর্ণ স্বচ্ছতা ও ন্যায়পরায়ণতা বজায় রাখবো।</p>
+        <p className='text-justify p-5'> <span className='font-extrabold'>দায়িত্বশীলতা: </span>শিশুদের জীবনমান উন্নয়নে আমাদের কাজের প্রতি দায়িত্বশীল থাকবো এবং সম্পদের যথাযথ ব্যবহার নিশ্চিত করবো।</p>
+        <p className='text-justify p-5'> <span className='font-extrabold'>সহযোগিতা ও সংহতি </span>স্থানীয় ও আন্তর্জাতিকভাবে অন্যান্য সংস্থার সাথে সহযোগিতা ও সমন্বয় করবো যাতে শিশুদের উন্নতির জন্য সর্বোচ্চ সুযোগ তৈরি করা যায়।</p>
+        <p className='text-justify p-5'> <span className='font-extrabold'>শিক্ষা ও উন্নয়ন: </span>প্রতিটি শিশুকে মানসম্মত শিক্ষা ও বিকাশের সুযোগ প্রদানে অঙ্গীকারবদ্ধ থাকবো।</p>
+        <p className='text-justify p-5'> <span className='font-extrabold'>নিরাপত্তা ও সুরক্ষা:  </span>শিশুদের জন্য একটি নিরাপদ ও সুরক্ষিত পরিবেশ নিশ্চিত করবো।</p>
+        <p className='p-10 font-extrabold text-justify' >আমরা দৃঢ় প্রতিজ্ঞ যে আমাদের প্রচেষ্টা সুবিধাবঞ্চিত শিশুদের জীবনে নতুন আশার আলো জ্বালাবে এবং তাদের ভবিষ্যৎ উজ্জ্বল করবে <br />শিশুদের আশার জন্য কাজ করবো, যতদিন না পৃথিবী একটি সুন্দর ও সমানাধিকারপূর্ণ স্থান হয়ে ওঠে।</p>
+    </div>
+    );
+};
+
+export default page;
