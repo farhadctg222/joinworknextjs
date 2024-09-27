a9000f567d8fc024e2fcd1d552b8af922522e44b
farhadctg222 <farhadctg222@gmail.com>
2024-09-27 18:10:14 +0600

    d

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -27,7 +27,7 @@ const  Login = () => {
               id="email"
               className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               placeholder="Enter your email"
-              value='email'
+              value=''
               required
             ></input>
           </div>
@@ -41,7 +41,7 @@ const  Login = () => {
               id="password"
               className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
               placeholder="Enter your password"
-              value='password'
+              value=''
               required></input>
         
           </div>
