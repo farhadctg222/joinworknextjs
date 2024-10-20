2483b8122bbfe2911b03e01ef67933ed4696d88c
farhadctg222 <farhadctg222@gmail.com>
2024-10-20 06:52:07 -0700

    d

--- app/api/apply/route.js
+++ app/api/apply/route.js
@@ -4,6 +4,7 @@ import { NextResponse } from "next/server"
 
 export const POST = async (request)=>{
     const data = await request.json()
+    
    
     const database = await Conectdb();
     const collections = await database.collection('data')
