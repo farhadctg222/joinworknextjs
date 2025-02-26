289ee91e33cd327f50b1114ae193e83cc12c26d6
Mohammad Farhad <134843834+farhadctg222@users.noreply.github.com>
2025-02-26 17:01:30 +0600

    Update page.jsxd

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,24 +2,23 @@ import React from 'react';
 import Dashbord from '../componet/Dashbord';
 
 
-//  const getServerSideProps = async ()=>{
+const getServerSideProps = async ()=>{
     
-//     const result = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/data`,
-//        {
-//           cache:'no-store'
-//        }
-//     );
-   
-//     const res = await result.json()
-//     return res
+  const result = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/data`,
+      {
+          cache:'no-store'
+      } 
+                            );
+       const res = await result.json()
+    return res
   
-// }
+}
 
 export const dynamic = 'force-dynamic'
 
 const getServerSideProps =  async() => {
-    // const user = await getAllData()
-    // console.log(user)
+     const user = await getAllData()
+    //console.log(user)
     
         
   
@@ -43,7 +42,7 @@ const getServerSideProps =  async() => {
 export default getServerSideProps;
 
 
-{/* <div className="container  overflow-x-auto mx-auto p-6 bg-gray-100">
+<div className="container  overflow-x-auto mx-auto p-6 bg-gray-100">
      <h1 className="text-2xl font-bold mb-4">User Dashboard</h1>
      <div className="overflow-x-auto rounded-lg border border-gray-200 shadow-lg">
          <table className="min-w-full w-1/4 border-collapse border-auto divide-y divide-gray-200">
@@ -75,5 +74,5 @@ export default getServerSideProps;
              </tbody>
          </table>
      </div>
-     </div> */}
+     </div> 
  
