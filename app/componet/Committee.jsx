6ecf0f04595bd7a0593bd3bd8f9a3c94008b8210
Mohammad Farhad <134843834+farhadctg222@users.noreply.github.com>
2025-02-26 14:09:22 +0600

    Update Committee.jsxfarhad

--- app/componet/Committee.jsx
+++ app/componet/Committee.jsx
@@ -24,7 +24,7 @@ const Committee = () => {
                     <Image src={pic3} className="w-full rounded-lg h-50 sm:rounded-none sm:rounded-l-lg" placeholder="blur" alt="md minhaz" ></Image></a>
                     <div className="p-5">
                         <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                           <a href="#">Mohammad Farhad Uddin</a>
+                  <title>         <a href="#">Mohammad Farhad Uddin</a></title>
                         </h3>
                         <span className="text-gray-500 dark:text-gray-400">CEO & Web Developer</span>
                         
