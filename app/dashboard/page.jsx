4ab6dc42818079d9391fe10bb419c3dd1c9a25ee
Mohammad Farhad <134843834+farhadctg222@users.noreply.github.com>
2025-02-26 19:27:11 +0600

    Update page.jsxd

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -36,11 +36,12 @@ const gets =  async() => {
        <div className="container  overflow-x-auto mx-auto p-6 bg-gray-100">
      <h1 className="text-2xl font-bold mb-4">User Dashboard</h1>
      <div className="overflow-x-auto rounded-lg border border-gray-200 shadow-lg">
-         <table className="min-w-full w-1/4 border-collapse border-auto divide-y divide-gray-200">
+         <table className="min-w-full w-1/4 border-collapse border-auto divide-y divide-gray-200"></table>
 
      </div> 
+        </div>
 
-
+       </div>
 
 </>
 
