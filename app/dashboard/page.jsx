201ada1a7d825e34e9844fa7a911b301b0018754
Mohammad Farhad <134843834+farhadctg222@users.noreply.github.com>
2025-02-26 18:41:13 +0600

    Update pagedd

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,68 +1,46 @@
-'use client'
+ import React from 'react';
+import Dashbord from '../componet/Dashbord';
 
-import Dashbord from '../componet/Dashbord
 
-const get = async ()=>{
-    
+const getServerSideProps = async ()=>{
+
+
   const result = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/data`,
-      {
-          cache:'no-store'
-      } 
-               ) ;           
-       const res = await result.json()
-    return res
- 
-}
-export const dynamic = 'force-dynamic'
+                             }
+
 
 const getServerSideProps =  async() => {
-     const user = await get()
+     const user = await getAllData()
+     
     //console.log(user)
-    
-        
-  
 
 
 
     return (
-        
+        <>
+
+       <div>
+       <Dashbord></Dashbord>
+       </div>
+
+
+
+
+ 
+
+      
+
+<div className="container  overflow-x-auto mx-auto p-6 bg-gray-100">
        <div className="container  overflow-x-auto mx-auto p-6 bg-gray-100">
      <h1 className="text-2xl font-bold mb-4">User Dashboard</h1>
      <div className="overflow-x-auto rounded-lg border border-gray-200 shadow-lg">
          <table className="min-w-full w-1/4 border-collapse border-auto divide-y divide-gray-200">
-             <thead className="bg-gray-50">
-                 <tr>
-                     <th scop='col' className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                     <th scop='col' className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
-                     <th scop='col' className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Mobile</th>
-                     <th scop='col' className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Donation Tk</th>
-                     <th scop='col' className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
-                 </tr>
-             </thead>
-             <tbody className="bg-white divide-y divide-gray-200">
-                 {user.map((user,index) => (
-                     <tr key={user._id}>
-                         <td scop='row' className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{index+1}</td>
-                         <td scop='row' className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{user.name}</td>
-                         <td scop='row' className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{user.email}</td>
-                         <td scop='row' className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{user.mobile}</td>
-                         <td scop='row' className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{user.donationAmount}</td>
-                         <td scop='row' className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{user.isDonar}</td>
-                         <td scop='row' className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">{user.isVolunteer}</td>
-                         <td scop='row' className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                             <button className="text-indigo-600 hover:text-indigo-900">Edit</button>
-                          
-                         </td>
-                     </tr>
-                 ))}
-             </tbody>
-         </table>
-     </div>
+
      </div> 
- 
 
 
 
+</>
 
 
  
@@ -71,5 +49,3 @@ const getServerSideProps =  async() => {
 };
 
 export default getServerSideProps;
-
-
